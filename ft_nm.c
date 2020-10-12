08352cc221568f24898e3fce98d17ffcdc151856
Julien THOMAS <juthomas@e2r3p12.42.fr>
2020-10-12 15:18:42 +0200

    [ADD] mac beg

--- ft_nm.c
+++ ft_nm.c
@@ -1,6 +1,49 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+
+
+void	handle_64(char *ptr)
+{
+	int						ncmds;
+	int						i;
+	struct	mach_header_64	*header;
+	struct	load_command	*lc;
+	struct	symtab_command	*sym;
+
+	header = (struct mach_header_64*)ptr;
+	ncmds = header->ncmds;
+	lc = (void*)ptr + sizeof(*header);
+	for (i = 0; i < ncmds; i++)
+	{
+		if (lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct systab_command *)lc;
+			//print_output(sym->nsyms, sym->symoff, sym->stroff);
+			break;
+		}
+		lc = (void*)lc + lc->cmdsize;
+	}
+
+}
+
+void	nm(char *ptr)
+{
+	int		magic_number;
+
+	magic_number = *(int*)ptr;
+	if (magic_number == MH_MAGIC_64)
+	{
+		handle_64(ptr);
+	}
+
+
+}
 
 void	ft_putchar(char c)
 {
@@ -17,33 +60,35 @@ void	ft_putstr(char *str)
 
 int main(int argc, char **argv)
 {
-	int fd;
-	char *str;
+	int			fd;
+	char		*ptr;
+	struct stat	buf;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-		{
-			ft_putstr("Fd error\n");
-			return (-1);
-		}
-		int i = 0;
-		char tmp[1];
-		while (read(fd, tmp, 1) > 0)
-		{
-			if (tmp[0] > 32 && tmp[0] < 126)
-			{
-				while (tmp[0] > 32 && tmp[0] < 126)
-				{
-					ft_putchar(tmp[0]);
-					if (read(fd, tmp, 1) <= 0)
-					{
-						break;
-					}
-				}
-				ft_putchar('\n');
-			}
-		}
+		fprintf(stderr, "Please give me an arg\n");
+		return (EXIT_FAILURE);
 	}
+	if ((fd = open(argv[1], O_RDONLY)) < 0)
+	{
+		perror("open");
+		return (EXIT_FAILURE);
+	}
+	if (fstat(fd, &buf) < 0)
+	{
+		perror("fstat");
+		return (EXIT_FAILURE);
+	}
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+	{
+		perror("mmap");
+		return (EXIT_FAILURE);
+	}
+	nm(ptr);
+	if (munmap(ptr, buf.st_size))
+	{
+		perror("munmap");;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
